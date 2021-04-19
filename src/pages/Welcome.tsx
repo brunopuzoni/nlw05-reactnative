bb9b556d32cdc39563670c06e6a26db3fc0fc948
Bruno Puzoni <bpuzoni@gmail.com>
2021-04-19 11:09:27 -0300

    Aula 01

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Text, SafeAreaView, Image, TouchableOpacity, StyleSheet } from 'react-native';
+
+import wateringImg from '../assets/watering.png';
+import { Button } from '../components/Button';
+import colors from '../styles/colors';
+
+export function Welcome() {
+  return (
+    <SafeAreaView style={ styles.container }>
+      <Text style={ styles.title }>
+        Gerencie {'\n'}
+        suas plantas {'\n'}
+        de forma fácil
+      </Text>
+
+      <Image source={ wateringImg } style={ styles.image } />
+
+      <Text style={ styles.subtitle }>
+        Não esqueça mais de regar sua plantas.
+        Nós cuidamos de lembar você sempre que precisar.
+      </Text>
+
+      <Button title=">"/>
+
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: colors.heading,
+    marginTop: 38
+  },
+
+  subtitle: {
+    textAlign: 'center',
+    fontSize: 18,
+    paddingHorizontal: 20,
+    color: colors.heading
+  },
+
+  image: {
+    width: 292,
+    height: 284
+  }
+});
