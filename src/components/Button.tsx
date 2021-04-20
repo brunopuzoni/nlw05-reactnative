7fcde1bf5c71b90d4e661649d05357cf2bcadbcb
Bruno Puzoni <bpuzoni@gmail.com>
2021-04-20 19:05:12 -0300

    Aula 02

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
-import colors from '../styles/colors';
+import { 
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  TouchableOpacityProps
+} from 'react-native';
 
-interface ButtonProps {
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+interface ButtonProps extends TouchableOpacityProps {
   title: string;
 }
 
-export function Button({ title }: ButtonProps){
+export function Button({ title, ...rest }: ButtonProps) {
   return(
     <TouchableOpacity 
-      style={ styles.button } 
-      activeOpacity={0.7}
+      style={ styles.container }
+      { ...rest }
     >
-      <Text style={ styles.buttonText }>
+      <Text style={ styles.text }>
         { title }
       </Text>
     </TouchableOpacity>
@@ -20,18 +27,17 @@ export function Button({ title }: ButtonProps){
 }
 
 const styles = StyleSheet.create({
-  button: {
+  container: {
     backgroundColor: colors.green,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 16,
-    marginBottom: 10,
     height: 56,
-    width: 56
+    borderRadius: 16,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
 
-  buttonText: {
+  text: {
+    fontSize: 16,
     color: colors.white,
-    fontSize: 24
+    fontFamily: fonts.heading
   }
 })
